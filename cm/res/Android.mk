c09c5a4fb81c79fbad248ed6ea1db088d2451524
fusionjack <dogfight60-fusionjack@yahoo.de>
2015-08-19 22:32:16 +0200

    Revert "CMSDK: Utilize CMSDK_PACKAGE_ID."
    This reverts commit 125c8993ca8bce661a1fbca9adcdda68f88c05f5.

--- cm/res/Android.mk
+++ cm/res/Android.mk
@@ -20,13 +20,12 @@ include $(CLEAR_VARS)
 LOCAL_PACKAGE_NAME := org.cyanogenmod.platform-res
 LOCAL_CERTIFICATE := platform
 LOCAL_AAPT_FLAGS := --auto-add-overlay
+
 LOCAL_RESOURCE_DIR := $(addprefix $(LOCAL_PATH)/, res)
 
 # Tell aapt to create "extending (non-application)" resource IDs,
 # since these resources will be used by many apps.
-
-# 0x3f/one less than app id
-LOCAL_AAPT_FLAGS += -x 63
+LOCAL_AAPT_FLAGS := -x
 
 LOCAL_MODULE_TAGS := optional
 
